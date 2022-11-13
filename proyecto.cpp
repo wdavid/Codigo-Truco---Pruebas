851850b2ca94706c80ca03fcbb224d32859a2fd3
David Hernandez <00135222@uca.edu.sv>
2022-11-13 13:08:12 -0600

    Changes

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,60 +1,77 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
+int primerPais[4], segundoPais[4];
+float totalPrimPais, totalSegPais, total;
+string primpais, segpais;
+
 void pedirDatos();
+void calculos();
+void resultados();
 
 int main()
 {
+    cout <<"==========Bienvenido al simulador de guerra================" << endl;
     pedirDatos();
+    calculos();
+    resultados();
     return 0;
 }
 
 void pedirDatos(){
-    int eeuu[4], rusia[4];
-    double totalEEUU, totalRusia, total; // 1 solados, 2 tanques, 3 aviones, 4 barcos
-    //1 tanque = 50 soldados
-    //1 avion = 75 soldados
-    //1 barco = 250 soldados
-    cout << "===========EEUU vs Rusia================" << endl;
-    cout << "Ingrese los datos de EEUU: " << endl;
+     // 1 tanque = 50 soldados
+    // 1 avion = 75 soldados
+    // 1 barco = 250 soldados
+    cout << "Ingrese los datos que se solicitan."<<endl;
+    cout <<"==========================================================="<<endl;
+    cout <<"Ingresa el nombre del primer pais del enfrentamiento: ";
+    getline(cin, primpais);
+    cout <<"Ingresa el nombre del segundo pais del enfrentamiento: ";
+    getline(cin, segpais);
+    cout << "===========" << primpais << " vs " << segpais << "=============" << endl;
+    //datos primer pais
+    cout << "Ingrese los datos de "<<primpais<<":" << endl;
     cout << "Numero de soldados: ";
-    cin >> eeuu[1];
+    cin >> primerPais[0];
     cout << "Numero de tanques: ";
-    cin >> eeuu[2];
+    cin >> primerPais[1];
     cout << "Numero de aviones: ";
-    cin >> eeuu[3];
+    cin >> primerPais[2];
     cout << "Numero de barcos: ";
-    cin >> eeuu[4];
+    cin >> primerPais[3];
     cout << "========================= " << endl;
-    cout << "Ingrese los datos de Rusia: " << endl;
-     cout << "Numero de soldados: ";
-    cin >> rusia[1];
+    //datos segundo pais
+    cout << "Ingrese los datos de "<<segpais<<":" << endl;
+    cout << "Numero de soldados: ";
+    cin >> segundoPais[0];
     cout << "Numero de tanques: ";
-    cin >> rusia[2];
+    cin >> segundoPais[1];
     cout << "Numero de aviones: ";
-    cin >> rusia[3];
+    cin >> segundoPais[2];
     cout << "Numero de barcos: ";
-    cin >> rusia[4];
-    totalEEUU = eeuu[1] + eeuu[2]*50 + eeuu[3]*75 + eeuu[4]*250;
-    totalRusia = rusia[1] + rusia[2]*50 + rusia[3]*75 + rusia[4]*250;
-    total = totalEEUU + totalRusia;
-    cout << endl;
-    cout << endl;
+    cin >> segundoPais[3];
+};
 
-    if (totalEEUU > totalRusia){
-        cout << "EEUU tiene mas posibilidades de ganar la guerra con el " << (totalEEUU/total)*100 << "%" << endl;
-        cout << "Mientras que Rusia tiene posibilidades del " << (totalRusia/total)*100 << "%" << endl;
-        cout << endl;
-    }
-    else if (totalRusia > totalEEUU){
-        cout << "Rusia tiene mas posibilidades de ganar la guerra con el " << (totalRusia/total)*100 << "%" << endl;
-        cout << "Mientras que EEUU tiene posibilidades del " << (totalEEUU/total)*100 << "%" << endl;
-        cout << endl;
-    }
-    else{
-        cout << "Ambos paises tienen las mismas posibilidades de ganar la guerra, EEUU " <<(totalEEUU/total)*100 << "% y Rusia " << (totalRusia/total)*100 << "%" << endl;
-        cout << endl;
-    }
-}
 
+void calculos(){
+    totalPrimPais = primerPais[0] + (primerPais[1]*50) + (primerPais[2]*75) + (primerPais[3]*250);
+    totalSegPais = segundoPais[0] + (segundoPais[1]*50) + (segundoPais[2]*75) + (segundoPais[3]*250);
+    total = totalPrimPais + totalSegPais;
+};
+
+void resultados(){
+    if (totalPrimPais > totalSegPais){
+        cout <<  primpais << " tiene mas posibilidades de ganar la guerra con el " << (totalPrimPais/total)*100 << "%" << endl;
+        cout << "Mientras que " << segpais << " tiene posibilidades del " << (totalSegPais/total)*100 << "%" << endl;
+        cout << endl;
+    }else if (totalSegPais > totalPrimPais){
+        cout << segpais <<" tiene mas posibilidades de ganar la guerra con el " << (totalSegPais/total)*100 << "%" << endl;
+        cout << "Mientras que " << primpais << " tiene posibilidades del " << (totalPrimPais/total)*100 << "%" << endl;
+        cout << endl;
+    }else{
+        cout << "Ambos paises tienen las mismas posibilidades de ganar la guerra, con el " <<(totalPrimPais/total)*100 << "% cada uno.\n";
+        cout << endl;
+    }
+};
